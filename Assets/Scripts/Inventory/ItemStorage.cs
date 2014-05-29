1cfe16966927355b741edd50f7775a396f2fbdce
Gapti <mark@marks-iMac.local>
2014-05-29 10:29:43 +0100

    Renamed Storage to item storage
    Renamed Storage to item storage and added a bounds check

--- Assets/Scripts/Inventory/ItemStorage.cs
+++ Assets/Scripts/Inventory/ItemStorage.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 
 [System.Serializable]
-public class Storage : MonoBehaviour {
+public class ItemStorage : MonoBehaviour {
 	
 	private bool _showGUI = false;
 
@@ -96,6 +96,11 @@ public class Storage : MonoBehaviour {
 	
 	public bool Add(int pos, Item item)
 	{
+
+		if(pos >= Items.Length)
+		{
+			return false;
+		}
 
 		if(item == null)
 		{
